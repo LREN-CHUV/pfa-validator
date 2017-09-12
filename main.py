71deffb58a81f3b6d4e876f2ffac2669d32eba74
James Nolan <james.nolan@chuv.ch>
2017-09-12 18:15:53 +0200

    Read PFA file path from command line

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import sys
 import titus.version
 
 print "Hello, world!"
 print "Titus is installed with version:"
 print titus.version.__version__
+
+if (len(sys.argv) < 2):
+  print "Program expects exactly one argument, that should be the path to the PFA file to validate"
+  print "* Example: python main.py path_to_pfa.json"
+  sys.exit()
+
+print "You called the program with argument", sys.argv[1]
