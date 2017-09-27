442a3da13968d6f6b5e91a3d3ba1da679c9ecaf7
James Nolan <james.nolan@chuv.ch>
2017-09-27 17:16:35 +0200

    No need to execute the main python as a script anymore, because main.py doesn't export anything

--- src/main.py
+++ src/main.py
@@ -7,33 +7,29 @@ from utils import print_ok, print_error
 from JSONPFAValidator import JSONPFAValidator
 
 
-if __name__ == '__main__':
-    # file executed directly, so we load the file provided as environment variable and run validator
-    # with PFA if the file exists
+# Check that a PFA_PATH variable was set
+PFA_PATH = os.environ.get('PFA_PATH')
 
-    # Check that a PFA_PATH variable was set
-    PFA_PATH = os.environ.get('PFA_PATH')
+if PFA_PATH is None:
+    print_error("Program expects an PFA_PATH environment variable that contains the path to "
+                "PFA file to validate. Please refer to the associated README.md file for "
+                "detailed usage instructions")
+    sys.exit(1)
 
-    if PFA_PATH is None:
-        print_error("Program expects an PFA_PATH environment variable that contains the path to "
-                    "PFA file to validate. Please refer to the associated README.md file for "
-                    "detailed usage instructions")
-        sys.exit(1)
+# Check that the pfa path passed by the user exists
+if not os.path.exists(PFA_PATH):
+    print_error("The path you provided does not exist:" + os.path.abspath(PFA_PATH))
+    sys.exit(1)
 
-    # Check that the pfa path passed by the user exists
-    if not os.path.exists(PFA_PATH):
-        print_error("The path you provided does not exist:" + os.path.abspath(PFA_PATH))
-        sys.exit(1)
+PFA_STRING = None
+with open(PFA_PATH, 'r') as content_file:
+    PFA_STRING = content_file.read()
 
-    PFA_STRING = None
-    with open(PFA_PATH, 'r') as content_file:
-        PFA_STRING = content_file.read()
+VALIDATOR = JSONPFAValidator(PFA_STRING)
+(VALID, REASON) = VALIDATOR.validate()
 
-    VALIDATOR = JSONPFAValidator(PFA_STRING)
-    (VALID, REASON) = VALIDATOR.validate()
+if not VALID:
+    print_error(REASON)
+    sys.exit(1)
 
-    if not VALID:
-        print_error(REASON)
-        sys.exit(1)
-
-    print_ok("This is a valid PFA file!")
+print_ok("This is a valid PFA file!")
