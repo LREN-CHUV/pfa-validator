ad3de5135217f396b178182d7c9bb8e9b79e6380
Martin Breskvar <martin.breskvar@ijs.si>
2018-03-05 15:57:40 +0000

    added a condition in the PostgreSQL sql_template

---
+++ go.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker run \
+    --env PFA_PATH="model.txt" \
+    --env FEATURES_DB_HOST=db \
+    --env FEATURES_DB_PORT=5432 \
+    --env FEATURES_DB_NAME=sample \
+    --env FEATURES_DB_USER=sample \
+    --env FEATURES_DB_PASSWORD=featurespwd \
+    --env FEATURES_DB_TABLE=features \
+    hbpmip/pfa-validator:0.10.1-0
